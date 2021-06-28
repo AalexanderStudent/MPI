91b6b92335704b128f8d1b0588253294bc54e936
AalexanderStudent <aalexanderst@gmail.com>
2021-06-28 17:33:20 +0300

    minor improvement

--- lab 1/MPI1.cpp
+++ lab 1/MPI1.cpp
@@ -1,7 +1,6 @@
 ﻿// MPI1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include "pch.h"
 #include <stdio.h>
 #include <mpi.h>
 #include <iostream>
